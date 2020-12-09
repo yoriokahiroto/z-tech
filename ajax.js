c26a91ab65b57e9f8f1edcb1baf30fe83cccbb26
matsubara <matsubara@jwcc.co.jp>
2020-12-10 00:08:52 +0900

    ajax処理・確認用html追加

---
+++ ajax.js
@@ -0,0 +1,18 @@
+function submitForm( data ){
+	$('#answer').text('通信中...');
+	$.ajax({
+		url: 'https://j2.jwcc.io',
+		type: 'POST',
+		dataType: 'json',
+		data: data,
+		timeout: 5000,
+	})
+	.done(function(data) {
+		alert('データの送信が完了しました。');
+		console.log(data);
+		$('#answer').html('<span style="color: red;">合計値は' + data.sum + 'です</span>');
+	})
+	.fail(function() {
+		alert('データの通信に失敗しました。');
+	});
+}
